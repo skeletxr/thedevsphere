404eea6607f5884d7d0c113b6441d46532eee898
saket <saketrazdan1@gmail.com>
2025-12-22 01:57:40 +0530

    test

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = ({scrollToTestimonials}) => {
   return (
     <div className="mt-20 tracking-wide" ref={scrollToTestimonials}>
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">
-        What People are saying
+        What People are saying.
       </h2>
       <div className="flex flex-wrap justify-center">
         {testimonials.map((testimonial, index) => (
